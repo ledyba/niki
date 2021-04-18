e5147fd4a7a5e4b983e7c33c9c827bd53febe76c
Kaede Fujisaki <kaede@hexe.net>
2021-04-18 19:31:12 +0900

    add init script

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+ROOT_DIR="$(cd "$(readlink -f "$(dirname "$0")")" && cd .. && pwd)"
+cd "${ROOT_DIR}" || exit 1
+
+set -eu
+set -o pipefail
+
+docker-compose run \
+  --rm \
+  -e 'FLYWAY_CONFIG_FILES=/flyway/conf/flyway.conf' \
+  flyway migrate
